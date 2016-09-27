151edfb54acfb79e7e31ceb3afa38d2f3dd581c9
Karin Kubischta <karin.kubischta@gmail.com>
2016-09-27 16:55:06 -0700

    index shows everything, show shows one

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,17 @@
 module TasksHelper
+  def show_description
+    content_tag(:p) do
+      @this_task[:description]
+    end
+  end
+
+  def show_status
+    content_tag(:p) do
+      @this_task[:completion_status]
+    end
+  end
+
+  def show_date
+    @this_task[:completion_date].to_s
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
   get '/tasks' => 'tasks#index'
+  get '/tasks/:id' => 'tasks#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
