3442bfc7c15fca8b3c22d162e6ef075648b77313
Karin Kubischta <karin.kubischta@gmail.com>
2016-09-28 15:54:20 -0700

    added button on uncompleted tasks, doesn't work yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,15 @@ class TasksController < ApplicationController
 
     redirect_to tasks_path
   end
+
+  def completed
+    # This doesn't work because once you press the button theres no more ID in the params. Not sure why. 
+    completed_task = Task.find(params[:id])
+    completed_task.completed = true
+    completed_task.completed_at = DateTime.now
+
+    redirect_to task_path(params[:id])
+  end
 end
 
 # create a controller for your Tasks
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -11,8 +11,10 @@ module TasksHelper
         "Completed: " + @this_task[:completed_at].to_s
       end
     else
-      content_tag(:p) do
-        "Not Yet Completed"
+      # label_tag do
+      #   concat check_box_tag 'completed'
+      form_tag '/task' do
+        submit_tag 'Completed!'
       end
     end
   end
