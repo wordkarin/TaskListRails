ab41212cdd0edc76f5bf957bb79480829e82f41a
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-23 12:13:22 -0700

    added two tests for sessions controller, fixtures for user

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def login_a_user
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+    get :create,  {provider: "github"}
+  end
+
+  test "Can Create a user" do
+    assert_difference('User.count', 1) do
+      login_a_user
+      assert_response :redirect
+      assert_redirected_to tasks_path
+    end
+  end
+
+  test "If a user logs in twice it doesn't create a 2nd user" do
+    assert_difference('User.count', 1) do
+      login_a_user
+    end
+    assert_no_difference('User.count') do
+      login_a_user
+      assert_response :redirect
+      assert_redirected_to tasks_path
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,18 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def setup
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited to use the mock authentication hash.
+    # A request to /auth/provider will redirect immediately to /auth/provider/callback.
+    OmniAuth.config.test_mode = true
+
+    # The mock_auth configuration allows you to set per-provider (or default) authentication
+    # hashes to return during testing.
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github', uid: '123545', info: { email: "a@b.com", name: "Ada" }
+      })
+  end
 end
